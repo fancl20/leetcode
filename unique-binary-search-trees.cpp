ad96be9a1ab3fa3036a3423eb2f13fbf0a5fa91c
fancl20 <fancl20@gmail.com>
2014-04-05 16:44:40 +0800

    http://oj.leetcode.com/problems/unique-binary-search-trees/

---
+++ unique-binary-search-trees.cpp
@@ -0,0 +1,13 @@
+class Solution {
+	public:
+		int numTrees(int n) {
+			int res(1);
+			for(int i(0); i<n; ++i) {
+				int tmp = __gcd(res, i+2);
+				res /= tmp;
+				tmp = (i+2)/tmp;
+				res *= 2*(2*i+1)/tmp;
+			}
+			return res;
+		}
+};
